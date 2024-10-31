1e6f0dde130e2b440dfbaead35c712dee445367b
Ítalo Lemos Gasparo <71532764+ItaloLemosGasparo@users.noreply.github.com>
2024-10-31 13:58:43 -0300

    Update Queries.cs

--- Globals/Queries.cs
+++ Globals/Queries.cs
@@ -25,5 +25,57 @@ namespace Globals
                 }
             }
         }
-    }
+
+        public static int ExecuteProcedure(string procedure, SqlParameterCollection lstParametros, SqlParameter idOutput = null, Ambiente ambiente = Ambiente.Producao)
+        {
+            int id = 0;
+            MsgRetornoClasse = "OK";
+        
+            SegCriptografia crypto = new SegCriptografia(SegCriptografia.CryptoTypes.encTypeDES);
+            string cnx = crypto.Decrypt(CieloDBConnection.strCnxDBCieloSolucion(ambiente), "Key_str.Cnx.DB.CieloSolucion");
+        
+            using (SqlConnection connection = new SqlConnection(cnx))
+            {
+                try
+                {
+                    connection.Open();
+        
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+                        SqlDataAdapter adapter = new SqlDataAdapter();
+        
+                        cmd.Connection = connection;
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.CommandText = procedure;
+        
+                        cmd.Parameters.Clear();
+        
+                        foreach (var item in lstParametros.Cast<SqlParameter>().ToList())
+                        {
+                            cmd.Parameters.Add(item.ParameterName, item.SqlDbType).Value = item.Value;
+                        }
+        
+                        if (idOutput != null)
+                            cmd.Parameters.Add(idOutput);
+        
+                        cmd.CommandTimeout = 1000;
+                        id = cmd.ExecuteNonQuery();
+        
+                        if (idOutput != null)
+                            id = Convert.ToInt32(idOutput.Value);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MsgRetornoClasse = "Erro ao acessar a base de dados. Detalhe do Erro: \n" + ex.Message;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+        
+            return id;
+        }
+    }    
 }
